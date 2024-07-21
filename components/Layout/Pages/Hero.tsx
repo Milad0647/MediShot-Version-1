57cd332d2b115777203a3edd3fef29ecf6263ec2
Milad0647 <milad.aghili@outlook.com>
2024-07-21 16:35:09 +0330

    Update Hero.tsx

--- components/Layout/Pages/Hero.tsx
+++ components/Layout/Pages/Hero.tsx
@@ -39,6 +39,8 @@ function Hero() {
                                         strings: [
                                             "MediShots",
                                         ],
+                                        autoStart: true,
+                                        loop: true,
                                     }}
                                 />
                             </h1>
